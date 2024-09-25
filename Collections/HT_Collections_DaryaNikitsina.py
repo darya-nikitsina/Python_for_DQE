f4e3f42c393f28e91965d89ffc262c788ef0fb58
Darya Nikitsina <nikitsina.darya@gmail.com>
2024-09-25 21:07:35 +0300

    Second task has been fully complited.

--- Collections/HT_Collections_DaryaNikitsina.py
+++ Collections/HT_Collections_DaryaNikitsina.py
@@ -28,5 +28,49 @@ for d in range(dict_quantity):
     # collect all previously created dicts into one list of dicts
     dicts_list.append(letter_number_dict)
 
-# print the result
+# print intermediate result
 print(dicts_list)
+
+# Get previously generated list of dicts and create one common dict:
+# if dicts have same key, we will take max value, and rename key with dict number with max value
+# if key is only in one dict - take it as is
+
+# initiate a dictionary to store max values and keys for them
+max_values = {}
+
+# for an index and dictionary in the list of dicts
+for index, dictionary in enumerate(dicts_list):
+    # dict number is greater by one than the index (because first index is 0)
+    dict_num = index + 1
+
+    # for key and value in a view of key-value pairs (as tuples in the list)
+    for key, value in dictionary.items():
+        # if key is not on the dict of max values
+        if key not in max_values:
+            # place value and dict_num to the dict
+            max_values[key] = (value, dict_num)
+        # if key is on the dict
+        else:
+            max_value, max_dict = max_values[key]
+            # and if the value for the key is higher than previously added value
+            if value > max_value:
+                # replace the value with higher one
+                max_values.update({key: (value, dict_num)})
+
+# initiate the common dict for all dicts from the list
+common_dict = {}
+
+# for key-value pair (where value is a tuple of value and dict_num) in the max_values dict
+for key, (value, dict_num) in max_values.items():
+    # if number of the same keys (sum of occurrences of one key) more than 1
+    if sum(key in dictionary for dictionary in dicts_list) > 1:
+        # add combination of key and dict_num from max_values as a key, and value as a value
+        # to the common_dict
+        common_dict[f'{key}_{dict_num}'] = value
+    # in other case
+    else:
+        # add key from the max_values as a key, and value as a value to the common_dict
+        common_dict[key] = value
+
+# print final result
+print(common_dict)
